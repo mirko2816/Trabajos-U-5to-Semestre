6de9f39c1b3f378e5c32da4d581ab0533f497726
Mirco <mirkosair28@gmail.com>
2024-09-08 13:49:17 -0500

    App Calculadora de intereses V0.1.0

---
+++ AppCalculadoraIntereses/AppCalculadoraIntereses/InteresSimpleWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppCalculadoraIntereses
+{
+    /// <summary>
+    /// Lógica de interacción para InteresSimpleWindow.xaml
+    /// </summary>
+    public partial class InteresSimpleWindow : Window
+    {
+        public InteresSimpleWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CalcularInteres_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double monto = double.Parse(MontoTextBox.Text);
+                double tasa = double.Parse(TasaTextBox.Text) / 100;
+                int tiempo = int.Parse(TiempoTextBox.Text);
+
+                double interes = monto * tasa * tiempo;
+
+                ResultadoLabel.Content = $"Interés Simple: S/ {interes}";
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Por favor, ingresa valores válidos.");
+            }
+        }
+    }
+}
---
+++ AppCalculadoraIntereses/AppCalculadoraIntereses/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppCalculadoraIntereses
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InteresSimple_Click(object sender, RoutedEventArgs e)
+        {
+            // Abrir la ventana de cálculo de interés simple
+            InteresSimpleWindow simpleWindow = new InteresSimpleWindow();
+            simpleWindow.Show();
+            this.Close(); // Cierra el menú principal
+        }
+
+        private void InteresCompuesto_Click(object sender, RoutedEventArgs e)
+        {
+            // IMPLEMENTAREMOS LA FUNCIONALIDAD DEL INTERES COMPUESTO PRONTO
+        }
+    }
+}
